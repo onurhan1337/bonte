2b64080f65454b12c1c2245e6c50c7f0fd2e07ad
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2023-12-16 12:00:18 +0300

    feat: add loading state to comment submit button

--- components/comment/form.tsx
+++ components/comment/form.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { useSession } from "next-auth/react";
+import LoadingSpinner from "../shared/icons/loading-spinner";
 
 type CommentFormProps = {
   text: string;
@@ -9,9 +11,16 @@ type CommentFormProps = {
 export default function CommentForm({
   text,
   setText,
-  onSubmit,
+  onSubmit: handleSubmit,
 }: CommentFormProps) {
   const { data: session } = useSession();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = async (e: React.FormEvent) => {
+    setIsLoading(true);
+    await handleSubmit(e);
+    setIsLoading(false);
+  };
 
   return (
     <form onSubmit={onSubmit}>
@@ -27,11 +36,14 @@ export default function CommentForm({
         value={text}
         disabled={!session}
       />
-
       <div className="flex items-center mt-4">
         {session && (
-          <button className="py-2 px-4 rounded bg-blue-600 text-white disabled:opacity-40 hover:bg-blue-700">
-            Gönder
+          <button
+            type="submit"
+            className="py-2 px-8 w-36 h-10 inline-flex justify-center items-center rounded-full bg-blue-600 text-white disabled:opacity-50 hover:bg-blue-700 disabled:cursor-not-allowed"
+            disabled={isLoading || !text}
+          >
+            {isLoading ? <LoadingSpinner /> : "Gönder"}
           </button>
         )}
       </div>
